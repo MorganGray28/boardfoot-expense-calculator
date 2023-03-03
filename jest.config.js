6e2a3bff6bed08d45e0c2eeb63a9565fb46a1d97
robert <robertmgray2828@gmail.com>
2023-03-03 14:08:59 -0800

    fix jest bug that would error on toBeInDocument test method

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,8 @@ const createJestConfig = nextJest({
 // Add any custom config to be passed to Jest
 /** @type {import('jest').Config} */
 const customJestConfig = {
+	setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+
 	// Add more setup options before each test is run
 	// setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 
