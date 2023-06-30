efd4436d381dc082ad75a6330c52485103942601
MorganGray28 <robertmgray2828@gmail.com>
2023-06-30 00:22:16 -0700

    Add delete lumber expense functionality

--- src/components/LumberExpenseListItem.tsx
+++ src/components/LumberExpenseListItem.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import styles from '../styles/LumberExpenseListItem.module.scss';
 import { calculateBoardFeet, calculateCostFromBF } from '../utils/calculationsUtils';
+import { trpc } from '../utils/trpc';
 
 interface PropTypes {
+	id: string;
 	name?: string | null;
 	species: string;
 	numOfPieces: number;
@@ -17,6 +19,7 @@ interface PropTypes {
 // TODO: Style the cost value to a different color to help separate and accent itself
 
 export default function LumberExpenseListItem({
+	id,
 	name,
 	species,
 	numOfPieces,
@@ -28,6 +31,14 @@ export default function LumberExpenseListItem({
 }: PropTypes) {
 	let boardFeet = calculateBoardFeet({ numOfPieces, thickness, width, length });
 	let cost = calculateCostFromBF({ boardFeet, price, tax });
+	const ctx = trpc.useContext();
+	const deleteLumber = trpc.lumber.deleteDimensionLumber.useMutation({
+		onSettled: () => ctx.user.getProjectsById.invalidate(),
+	});
+
+	function handleDelete() {
+		deleteLumber.mutate(id);
+	}
 
 	return (
 		<div className={styles.container}>
@@ -40,6 +51,9 @@ export default function LumberExpenseListItem({
 				<p className={styles.boardFeet}>{boardFeet} BF</p>
 				<p className={styles.cost}>${cost}</p>
 			</div>
+			<button onClick={handleDelete} disabled={deleteLumber.isLoading}>
+				Delete
+			</button>
 		</div>
 	);
 }
