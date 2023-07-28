6250a25df05ac5a95bf7bd9c00941689e82ebc23
MorganGray28 <robertmgray2828@gmail.com>
2023-07-28 10:44:51 -0700

    add id to props

--- src/components/ConsumableListItem.tsx
+++ src/components/ConsumableListItem.tsx
@@ -4,9 +4,10 @@ import styles from '../styles/ConsumableListItem.module.scss';
 interface PropTypes {
 	name: string;
 	cost: number;
+	id: string;
 }
 
-export default function ConsumableListItem({ name, cost }: PropTypes) {
+export default function ConsumableListItem({ name, cost, id }: PropTypes) {
 	return (
 		<div className={styles.container}>
 			<input className={styles.checkbox} type='checkbox' />
