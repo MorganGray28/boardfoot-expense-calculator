a80612dcf2ffc204f925d43f12d92df3373f04d0
MorganGray28 <robertmgray2828@gmail.com>
2023-08-17 16:00:56 -0700

    fixed cost value from displaying more than two decimal places

--- src/components/ProjectFormListItem.tsx
+++ src/components/ProjectFormListItem.tsx
@@ -16,7 +16,7 @@ function ProjectFormListItem({ name, species, cost, id, values, handleClick }: P
 		<li className={styles.projectListItem} onClick={() => handleClick(id, values)}>
 			<p className={styles.projectName}>{name}</p>
 			{species.length > 0 && <p className={styles.projectSpecies}>{species.join(', ')}</p>}
-			<p className={styles.projectCost}>{cost ? '$' + cost : '$0'}</p>
+			<p className={styles.projectCost}>{cost ? '$' + cost.toFixed(2) : '$0'}</p>
 		</li>
 	);
 }
