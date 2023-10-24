3cdb8810fc3efe430b3da3c7d3de119c7660dd02
robert <robertmgray2828@gmail.com>
2023-10-23 18:21:50 -0700

    fix type import eslint warning

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,4 +1,4 @@
-import { z } from 'zod';
+import type { z } from 'zod';
 import type { BoardFeetSchema } from '../server/trpc/router/lumber';
 
 export type BoardFeetType = z.infer<typeof BoardFeetSchema>;
