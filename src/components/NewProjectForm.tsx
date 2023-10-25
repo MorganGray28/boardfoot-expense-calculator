dec765e5704fda1af9d91249cc0d3990a297ed51
robert <robertmgray2828@gmail.com>
2023-10-24 23:16:26 -0700

    add new project form styles and move styles to scss file

--- src/components/NewProjectForm.tsx
+++ src/components/NewProjectForm.tsx
@@ -1,6 +1,7 @@
 import React, { type Dispatch, type SetStateAction, useState } from 'react';
 import { trpc } from '../utils/trpc';
 import type { ProjectType } from '../types/types';
+import styles from '../styles/NewProjectForm.module.scss';
 
 type PropsType = {
 	cancel: boolean;
@@ -10,6 +11,7 @@ type PropsType = {
 
 function NewProjectForm({ cancel, setActiveProject, setIsCreatingNewProject }: PropsType) {
 	const [newProjectName, setNewProjectName] = useState('');
+	const [newProjectDescription, setNewProjectDescription] = useState('');
 
 	const ctx = trpc.useContext();
 	const { mutateAsync: addNewProject, isLoading: isCreating } = trpc.project.createProject.useMutation({
@@ -18,6 +20,7 @@ function NewProjectForm({ cancel, setActiveProject, setIsCreatingNewProject }: P
 				setActiveProject(data);
 			}
 			setNewProjectName('');
+			setNewProjectDescription('');
 			setIsCreatingNewProject(false);
 			ctx.user.getProjectsById.invalidate();
 		},
@@ -26,11 +29,13 @@ function NewProjectForm({ cancel, setActiveProject, setIsCreatingNewProject }: P
 	function handleCancelNewProject() {
 		setIsCreatingNewProject(false);
 		setNewProjectName('');
+		setNewProjectDescription('');
 	}
 
+	// FIXME: revise the alert to something better when no name is entered
 	async function handleSubmitNewProject() {
 		if (newProjectName) {
-			await addNewProject(newProjectName);
+			await addNewProject({ name: newProjectName, description: newProjectDescription });
 		} else {
 			alert('Please fill out the Project Name input');
 		}
@@ -38,19 +43,43 @@ function NewProjectForm({ cancel, setActiveProject, setIsCreatingNewProject }: P
 
 	return (
 		<>
-			<label htmlFor='newProject'>New Project Name</label>
+			<label className={styles.textfieldLabel} htmlFor='newProjectName'>
+				Project Name
+			</label>
 			<input
-				id='newProject'
+				className={`${styles.searchInput} ${styles.nameInput}`}
+				id='newProjectName'
 				autoComplete='off'
 				type='text'
 				placeholder='enter project name'
 				value={newProjectName}
 				onChange={(e) => setNewProjectName(e.target.value)}
 			/>
-			{cancel && <button onClick={() => handleCancelNewProject()}>Cancel</button>}
-			<button disabled={isCreating} onClick={() => handleSubmitNewProject()}>
-				Create Project
-			</button>
+			<label htmlFor='newProjectDescription' className={styles.textfieldLabel}>
+				Project Description (optional)
+			</label>
+			<textarea
+				id='newProjectDescription'
+				name='projectDescription'
+				value={newProjectDescription}
+				onChange={(e) => setNewProjectDescription(e.target.value)}
+				className={`${styles.searchInput} ${styles.descriptionInput}`}
+			/>
+			<div className={styles.formButtonGroup}>
+				{cancel && (
+					<button className={styles.dangerBtn} type='button' onClick={handleCancelNewProject}>
+						Cancel
+					</button>
+				)}
+				<button
+					className={styles.approveBtn}
+					disabled={isCreating}
+					onClick={() => handleSubmitNewProject()}
+					type='submit'
+				>
+					Create Project
+				</button>
+			</div>
 		</>
 	);
 }
