6c559f88d4cc0e77fbd6fcd0a76de7ea670e1287
robert <robertmgray2828@gmail.com>
2023-12-09 20:23:27 -0800

    change BF calculator to position sticky

--- src/components/BoardFootCalculator.tsx
+++ src/components/BoardFootCalculator.tsx
@@ -162,212 +162,210 @@ function BoardFootCalculator({ handleModal }: PropsType) {
 						height={22}
 					/>
 				)}
-				<form onSubmit={handleSubmit}>
-					<div className={styles.boardfootContainer}>
-						<p className={styles.subheading}>Board Feet Calculator</p>
-						<div
-							className={
-								`${styles.labelInputGroup}` + (inputErrors.numOfPieces ? ` ${styles.inputError}` : '')
-							}
-						>
-							<div className={styles.inputLabelFlexGroup}>
-								<label className={styles.inputLabel} htmlFor='numOfPieces'>
-									No. of Pieces
-								</label>
-								{inputErrors.numOfPieces && <p className={styles.errorMessage}>*Required</p>}
-							</div>
-							<input
-								id='numOfPieces'
-								name='numOfPieces'
-								autoComplete='off'
-								value={values.numOfPieces || ''}
-								onChange={handleChange}
-								type='number'
-							/>
-						</div>
-						<div
-							className={`${styles.labelInputGroup}` + (inputErrors.thickness ? ` ${styles.inputError}` : '')}
-						>
-							<div className={styles.inputLabelFlexGroup}>
-								<label className={styles.inputLabel} htmlFor='thickness'>
-									Thickness
-								</label>
-								{inputErrors.thickness && <p className={styles.errorMessage}>*Required</p>}
-							</div>
-							<div className={styles.inputUnitsGroup}>
+				<div className={styles.stickyContainer}>
+					<form className={styles.calculatorForm} onSubmit={handleSubmit}>
+						<div className={styles.boardfootContainer}>
+							<p className={styles.subheading}>Board Feet Calculator</p>
+							<div
+								className={
+									`${styles.labelInputGroup}` + (inputErrors.numOfPieces ? ` ${styles.inputError}` : '')
+								}
+							>
+								<div className={styles.inputLabelFlexGroup}>
+									<label className={styles.inputLabel} htmlFor='numOfPieces'>
+										No. of Pieces
+									</label>
+									{inputErrors.numOfPieces && <p className={styles.errorMessage}>*Required</p>}
+								</div>
 								<input
-									id='thickness'
-									name='thickness'
+									id='numOfPieces'
+									name='numOfPieces'
 									autoComplete='off'
-									value={values.thickness || ''}
+									value={values.numOfPieces || ''}
 									onChange={handleChange}
 									type='number'
 								/>
-								<span>in</span>
 							</div>
-							<div className={styles.quarterThicknessContainer}>
-								<p
-									onClick={() => {
-										setValues({ ...values, thickness: 1 });
-										setInputErrors({ ...inputErrors, thickness: false });
-									}}
-								>
-									4/4
-								</p>
-								<p
-									onClick={() => {
-										setValues({ ...values, thickness: 1.5 });
-										setInputErrors({ ...inputErrors, thickness: false });
-									}}
-								>
-									6/4
-								</p>
-								<p
-									onClick={() => {
-										setValues({ ...values, thickness: 2 });
-										setInputErrors({ ...inputErrors, thickness: false });
-									}}
-								>
-									8/4
-								</p>
+							<div
+								className={
+									`${styles.labelInputGroup}` + (inputErrors.thickness ? ` ${styles.inputError}` : '')
+								}
+							>
+								<div className={styles.inputLabelFlexGroup}>
+									<label className={styles.inputLabel} htmlFor='thickness'>
+										Thickness
+									</label>
+									{inputErrors.thickness && <p className={styles.errorMessage}>*Required</p>}
+								</div>
+								<div className={styles.inputUnitsGroup}>
+									<input
+										id='thickness'
+										name='thickness'
+										autoComplete='off'
+										value={values.thickness || ''}
+										onChange={handleChange}
+										type='number'
+									/>
+									<span>in</span>
+								</div>
+								<div className={styles.quarterThicknessContainer}>
+									<p
+										onClick={() => {
+											setValues({ ...values, thickness: 1 });
+											setInputErrors({ ...inputErrors, thickness: false });
+										}}
+									>
+										4/4
+									</p>
+									<p
+										onClick={() => {
+											setValues({ ...values, thickness: 1.5 });
+											setInputErrors({ ...inputErrors, thickness: false });
+										}}
+									>
+										6/4
+									</p>
+									<p
+										onClick={() => {
+											setValues({ ...values, thickness: 2 });
+											setInputErrors({ ...inputErrors, thickness: false });
+										}}
+									>
+										8/4
+									</p>
+								</div>
+							</div>
+							<div
+								className={`${styles.labelInputGroup}` + (inputErrors.width ? ` ${styles.inputError}` : '')}
+							>
+								<div className={styles.inputLabelFlexGroup}>
+									<label className={styles.inputLabel} htmlFor='width'>
+										Width
+									</label>
+									{inputErrors.width && <p className={styles.errorMessage}>*Required</p>}
+								</div>
+								<div className={styles.inputUnitsGroup}>
+									<input
+										id='width'
+										name='width'
+										autoComplete='off'
+										value={values.width || ''}
+										onChange={handleChange}
+										type='number'
+									/>
+									<span>in</span>
+								</div>
+							</div>
+							<div
+								className={
+									`${styles.labelInputGroup} ${styles.length}` +
+									(inputErrors.length ? ` ${styles.inputError}` : '')
+								}
+							>
+								<div className={styles.inputLabelFlexGroup}>
+									<label className={styles.inputLabel} htmlFor='length'>
+										Length
+									</label>
+									{inputErrors.length && <p className={styles.errorMessage}>*Required</p>}
+								</div>
+								<div className={styles.inputUnitsGroup}>
+									<input
+										id='length'
+										name='length'
+										autoComplete='off'
+										value={values.length || ''}
+										onChange={handleChange}
+										type='number'
+									/>
+									<span>in</span>
+								</div>
+							</div>
+							<div className={styles.labelInputGroup}>
+								<p>Total:</p>
+								<p>{boardFeet} BF</p>
 							</div>
 						</div>
-						<div className={`${styles.labelInputGroup}` + (inputErrors.width ? ` ${styles.inputError}` : '')}>
-							<div className={styles.inputLabelFlexGroup}>
-								<label className={styles.inputLabel} htmlFor='width'>
-									Width
+						<button className={styles.clearButton} type='button' onClick={handleClearForm}>
+							Clear
+						</button>
+						<div className={styles.boardfootContainer}>
+							<p className={styles.subheading}>Cost</p>
+							<div className={styles.labelInputGroup}>
+								<label className={styles.inputLabel} htmlFor='name'>
+									Description <span className={styles.optionalSpan}>(optional)</span>
 								</label>
-								{inputErrors.width && <p className={styles.errorMessage}>*Required</p>}
-							</div>
-							<div className={styles.inputUnitsGroup}>
 								<input
-									id='width'
-									name='width'
+									type='text'
 									autoComplete='off'
-									value={values.width || ''}
+									id='name'
+									name='name'
 									onChange={handleChange}
-									type='number'
+									value={values.name}
+									placeholder='e.g. Table Legs'
 								/>
-								<span>in</span>
 							</div>
-						</div>
-						<div
-							className={
-								`${styles.labelInputGroup} ${styles.length}` +
-								(inputErrors.length ? ` ${styles.inputError}` : '')
-							}
-						>
-							<div className={styles.inputLabelFlexGroup}>
-								<label className={styles.inputLabel} htmlFor='length'>
-									Length
+							<div
+								className={`${styles.labelInputGroup}` + (inputErrors.species ? ` ${styles.inputError}` : '')}
+							>
+								<div className={styles.inputLabelFlexGroup}>
+									<label className={styles.inputLabel} htmlFor='species'>
+										Species
+									</label>
+									{inputErrors.species && <p className={styles.errorMessage}>*Required</p>}
+								</div>
+								<input
+									id='species'
+									type='text'
+									autoComplete='off'
+									name='species'
+									onChange={handleChange}
+									value={values.species}
+								/>
+							</div>
+							<div className={styles.labelInputGroup}>
+								<label className={styles.inputLabel} htmlFor='price'>
+									Price
 								</label>
-								{inputErrors.length && <p className={styles.errorMessage}>*Required</p>}
-							</div>
-							<div className={styles.inputUnitsGroup}>
 								<input
-									id='length'
-									name='length'
-									autoComplete='off'
-									value={values.length || ''}
-									onChange={handleChange}
+									id='price'
 									type='number'
+									autoComplete='off'
+									name='price'
+									onChange={handleChange}
+									value={values.price || ''}
 								/>
-								<span>in</span>
 							</div>
-						</div>
-						<div className={styles.labelInputGroup}>
-							<p>Total:</p>
-							<p>{boardFeet} BF</p>
-						</div>
-					</div>
-
-					<button className={styles.clearButton} type='button' onClick={handleClearForm}>
-						Clear
-					</button>
-
-					<div className={styles.boardfootContainer}>
-						<p className={styles.subheading}>Cost</p>
-						<div className={styles.labelInputGroup}>
-							<label className={styles.inputLabel} htmlFor='name'>
-								Description <span className={styles.optionalSpan}>(optional)</span>
-							</label>
-							<input
-								type='text'
-								autoComplete='off'
-								id='name'
-								name='name'
-								onChange={handleChange}
-								value={values.name}
-								placeholder='e.g. Table Legs'
-							/>
-						</div>
-
-						<div
-							className={`${styles.labelInputGroup}` + (inputErrors.species ? ` ${styles.inputError}` : '')}
-						>
-							<div className={styles.inputLabelFlexGroup}>
-								<label className={styles.inputLabel} htmlFor='species'>
-									Species
+							<div className={`${styles.labelInputGroup}`}>
+								<label className={styles.inputLabel} htmlFor='tax'>
+									Tax
 								</label>
-								{inputErrors.species && <p className={styles.errorMessage}>*Required</p>}
+								<div className={styles.inputUnitsGroup}>
+									<input
+										id='tax'
+										type='number'
+										autoComplete='off'
+										name='tax'
+										onChange={handleChange}
+										value={values.tax || ''}
+									/>
+									<span>%</span>
+								</div>
 							</div>
-							<input
-								id='species'
-								type='text'
-								autoComplete='off'
-								name='species'
-								onChange={handleChange}
-								value={values.species}
-							/>
-						</div>
-
-						<div className={styles.labelInputGroup}>
-							<label className={styles.inputLabel} htmlFor='price'>
-								Price
-							</label>
-							<input
-								id='price'
-								type='number'
-								autoComplete='off'
-								name='price'
-								onChange={handleChange}
-								value={values.price || ''}
-							/>
-						</div>
-
-						<div className={`${styles.labelInputGroup}`}>
-							<label className={styles.inputLabel} htmlFor='tax'>
-								Tax
-							</label>
-							<div className={styles.inputUnitsGroup}>
-								<input
-									id='tax'
-									type='number'
-									autoComplete='off'
-									name='tax'
-									onChange={handleChange}
-									value={values.tax || ''}
-								/>
-								<span>%</span>
+							<div className={`${styles.tax}`}>
+								<p>${preTax}</p>
+								<p>+ ${formattedTax || 0} tax</p>
+							</div>
+							<div className={styles.labelInputGroup}>
+								<p>Total Cost:</p>
+								<p>${totalCost}</p>
 							</div>
 						</div>
-
-						<div className={`${styles.tax}`}>
-							<p>${preTax}</p>
-							<p>+ ${formattedTax || 0} tax</p>
-						</div>
-
-						<div className={styles.labelInputGroup}>
-							<p>Total Cost:</p>
-							<p>${totalCost}</p>
-						</div>
-					</div>
-
-					<button disabled={!session} type='submit' className={styles.primaryButton}>
-						Add to Project
-					</button>
-					{!session && <p className={styles.helpMessage}>please sign in to add lumber to a project</p>}
-				</form>
+						<button disabled={!session} type='submit' className={styles.primaryButton}>
+							Add to Project
+						</button>
+						{!session && <p className={styles.helpMessage}>please sign in to add lumber to a project</p>}
+					</form>
+				</div>
 			</div>
 		</>
 	);
