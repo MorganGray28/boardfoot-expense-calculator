abfb7da9f80d908bd989cd7fc526d28c48653fcb
robert <robertmgray2828@gmail.com>
2024-01-30 15:34:21 -0800

    adjust sign in button and container styling

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -8,16 +8,16 @@ const signin: NextPage = () => {
 		<>
 			<div className={styles.signin}>
 				<div className={styles.container}>
-					<h1 className={styles.header}>Sign In</h1>
+					<h1 className={styles.header}>Sign in </h1>
 					<p className={styles.subheader}>Log in or create a new account</p>
 					<div className={styles.providerList}>
 						<button className={styles.providerButton} onClick={() => signIn('google', { callbackUrl: '/' })}>
 							<img src='/google-logo.svg' alt='google logo' />
-							Sign in with Google
+							Continue with Google
 						</button>
 						<button className={styles.providerButton} onClick={() => signIn('discord', { callbackUrl: '/' })}>
 							<img src='/discord-logo.svg' alt='discord logo' />
-							Sign in with Discord
+							Continue with Discord
 						</button>
 					</div>
 				</div>
