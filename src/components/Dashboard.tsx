d9ca12195f2f345075b6cd00390115dac5958a35
robert <robertmgray2828@gmail.com>
2024-02-04 23:37:33 -0800

    adjust styling for users without any projects created

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -3,8 +3,8 @@ import type { ProjectType } from '../types/types';
 import { ActiveProjectForm } from './ActiveProjectForm';
 import ExpenseAndConsumableGroup from './ExpenseAndConsumableGroup';
 import NewProjectForm from './NewProjectForm';
-import ProjectCostSummary from './ProjectCostSummary';
 import LoadingSpinner from './ui/LoadingSpinner/LoadingSpinner';
+import styles from '../styles/Dashboard.module.scss';
 
 type PropsType = {
 	projects: ProjectType[] | undefined;
@@ -56,7 +56,8 @@ export default function Dashboard({ projects, activeProject, setActiveProject, i
 		);
 	} else {
 		return (
-			<div>
+			<div style={{ paddingTop: '4rem' }}>
+				<p className={styles.newProjectMessage}>Create a New Project</p>
 				<NewProjectForm
 					cancel={false}
 					setActiveProject={setActiveProject}
