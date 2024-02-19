196aab979bcd397f44b09eda4913cc97f5325bb9
robert <robertmgray2828@gmail.com>
2024-02-18 21:43:00 -0800

    extract AuthCard component for auth container styling

---
+++ src/components/ui/AuthCard/AuthCard.tsx
@@ -0,0 +1,12 @@
+import React, { ComponentPropsWithoutRef } from 'react';
+import styles from './AuthCard.module.scss';
+
+function AuthCard({ children }: { children: React.ReactNode }) {
+	return (
+		<div className={styles.signin}>
+			<div className={styles.container}>{children}</div>
+		</div>
+	);
+}
+
+export default AuthCard;
