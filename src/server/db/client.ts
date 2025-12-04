b58fe2c8e715dd59187afb090bb8276ac5e70cfa
robert <robertmgray2828@gmail.com>
2025-12-03 22:40:20 -0800

    fix prisma v7 update bugs

--- src/server/db/client.ts
+++ src/server/db/client.ts
@@ -1,19 +1,26 @@
-import { PrismaClient } from "@prisma/client";
-
-import { env } from "../../env/server.mjs";
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { env } from '../../env/server.mjs';
 
 declare global {
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
+	// eslint-disable-next-line no-var
+	var prisma: PrismaClient | undefined;
 }
 
-export const prisma =
-  global.prisma ||
-  new PrismaClient({
-    log:
-      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-  });
+import { Pool } from 'pg';
 
-if (env.NODE_ENV !== "production") {
-  global.prisma = prisma;
+const connectionString = env.DATABASE_URL;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+
+export const prisma =
+	global.prisma ||
+	new PrismaClient({
+		adapter: adapter,
+		log: env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+	});
+
+if (env.NODE_ENV !== 'production') {
+	global.prisma = prisma;
 }
